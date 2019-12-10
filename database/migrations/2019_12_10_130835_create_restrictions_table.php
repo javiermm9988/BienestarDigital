793a399c3a43a2770bb42477482d8ebb6e11f162
JAVIER_MARTIN APPS1MA1819 <javier_martin_apps1ma1819@cev.com>
2019-12-10 14:34:20 +0100

    Initial commit

---
+++ database/migrations/2019_12_10_130835_create_restrictions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRestrictionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('restrictions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('app_id');
+            $table->dateTime('time');
+            $table->dateTime('from');
+            $table->dateTime('to');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('restrictions');
+    }
+}
---
+++ database/migrations/2019_12_10_130859_create_usages_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->dateTime('date');
+            $table->string('app');
+            $table->string('event');
+            $table->string('latitude');
+            $table->string('longitude');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usages');
+    }
+}
